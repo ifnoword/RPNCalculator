2c4c05b4bbdee27516442a34d4d526d8c1c2ff4f
MEI C <mei@wireless-nat-inside.uiowa.edu>
2014-02-02 18:41:05 -0600

    Initial Commit

---
+++ Caculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Caculator
+//
+//  Created by MEI C on 2/2/14.
+//  Copyright (c) 2014 MEI C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Caculator/CalculatorViewController.m
@@ -0,0 +1,29 @@
+//
+//  CalculatorViewController.m
+//  Caculator
+//
+//  Created by MEI C on 2/2/14.
+//  Copyright (c) 2014 MEI C. All rights reserved.
+//
+
+#import "CalculatorViewController.h"
+
+@interface CalculatorViewController ()
+
+@end
+
+@implementation CalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
